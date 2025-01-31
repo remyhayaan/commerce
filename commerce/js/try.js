6a849b5935fbba82c18492ce82a534aac3139f70
Nabukenya Rehemah <remmiehayaan@gmail.com>
2025-01-31 12:49:43 +0300

    first commit

---
+++ commerce/js/try.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetSection = document.querySelector(this.getAttribute('href'));
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
